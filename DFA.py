eda7dda6c54fd138e074a6540c57b1f1f3cc29c2
Ana Beatriz Kapps <anabeatrizkapps@ice.ufjf.br>
2022-07-03 16:42:13 -0300

    inserindo delimitadores no codigo

--- DFA.py
+++ DFA.py
@@ -21,6 +21,11 @@ for line in lines_symbols:
 
 TRANSITIONS = soup.find_all('transition')
 
+lines_delimeters = soup.find_all('delimiter')
+DELIMETERS = []
+for line in lines_delimeters:
+    DELIMETERS.append(line['value'])
+
 lines_finalState = soup.find_all('finalState')
 FINAL_STATE = None
 for line in lines_finalState:
@@ -40,6 +45,29 @@ class DFA:
         self.DELTA = self.populate_transition_function()
         self.START_STATE, self.ACCEPT_STATES = self.set_start_accept()
         self.CURRENT_STATE = None
+
+    def leituraarquivo(self):
+        palavras_arquivo = ""
+        vet_palavra = []
+        palavra = ""
+        rascunho_palavra = ""
+        cont = 0
+
+        with open("tests/teste.txt","r") as arquivo:
+            palavras_arquivo = arquivo.read()
+        for i in palavras_arquivo:
+            rascunho_palavra += i
+            achou_delimitador = [outros in DELIMETERS for outros in rascunho_palavra]
+            if (achou_delimitador[cont] == False):
+                palavra += i
+            else:
+                palavra += i
+                vet_palavra.append(palavra)
+                palavra = ""
+            cont += 1
+        vet_palavra.append(palavra)
+        print(vet_palavra)
+        return vet_palavra
 
     def set_start_accept(self):
         """Takes user input for START_STATE and ACCEPT_STATE, and checks if it's a valid state (if it belongs to Q)"""
@@ -100,7 +128,6 @@ class DFA:
                 return False
         return self.check_if_accept()
 
-
 if __name__ == "__main__":
     check = True
     print("\nMáquina de AFD.")
@@ -110,7 +137,8 @@ if __name__ == "__main__":
         if (choice == 1):
             sys.exit()
         elif (choice == 2):
-            input_string = list(input("Digite a palavra : "))
-            print("ACEITA" if machine.run_machine(input_string) else "REJEITADA")
+            palavra = machine.leituraarquivo()
+            print(palavra)
+            # print("ACEITA" if machine.run_machine(palavra) else "REJEITADA")
         else:
             check = False
