63fe9f153de8333c87abbb50e5f278a79815b09e
Ana Beatriz Kapps <anabeatrizkapps@ice.ufjf.br>
2022-07-10 17:15:03 -0300

    tirando o delimitador da palavra e add o arquivo de resultado

--- DFA.py
+++ DFA.py
@@ -52,39 +52,42 @@ class DFA:
         palavra = ""
         rascunho_palavra = ""
         cont = 0
+        verifica_delimitador_unico = False
 
-        with open("tests/teste.txt","r") as arquivo:
+        with open("tests/leitura.txt", "r") as arquivo:
             palavras_arquivo = arquivo.read()
         for i in palavras_arquivo:
             rascunho_palavra += i
             achou_delimitador = [outros in DELIMETERS for outros in rascunho_palavra]
-            if (achou_delimitador[cont] == False):
+            if ((len(palavras_arquivo) == 1) and achou_delimitador[cont] == True):
+                verifica_delimitador_unico = True
+                break
+            elif (achou_delimitador[cont] == False):
                 palavra += i
             else:
-                palavra += i
                 vet_palavra.append(palavra)
                 palavra = ""
             cont += 1
-        vet_palavra.append(palavra)
-        print(vet_palavra)
+        if(verifica_delimitador_unico != True):
+            vet_palavra.append(palavra)
         return vet_palavra
 
     def set_start_accept(self):
         """Takes user input for START_STATE and ACCEPT_STATE, and checks if it's a valid state (if it belongs to Q)"""
         # while (True):
         start = INITIAL_STATE
-        print("ESTADO INICIAL: {}".format(start))
+        # print("ESTADO INICIAL: {}".format(start))
         accept = FINAL_STATE
-        print("ESTADO FINAL: {}".format(accept))
+        # print("ESTADO FINAL: {}".format(accept))
 
         return start, accept
 
     def populate_states(self):
-        print("ESTADOS: {}".format(STATES))
+        # print("ESTADOS: {}".format(STATES))
         return STATES
 
     def populate_alphabet(self):
-        print("ALFABETO: {}".format(SYMBOLS))
+        # print("ALFABETO: {}".format(SYMBOLS))
         return SYMBOLS
 
     def populate_transition_function(self):
@@ -95,11 +98,11 @@ class DFA:
             for transition in TRANSITIONS:
                 transition_dict[transition['from']][transition['symbol']] = transition['to']
 
-        print("\nFUNÇÃO TRANSIÇÃO Q X SIGMA -> Q")
-        print("ESTADO ATUAL\tALFABETO DE ENTRADA\tPRÓXIMO ESTADO")
-        for key, dict_value in transition_dict.items():
-            for input_alphabet, transition_state in dict_value.items():
-                print("{}\t\t{}\t\t{}".format(key, input_alphabet, transition_state))
+        # print("\nFUNÇÃO TRANSIÇÃO Q X SIGMA -> Q")
+        # print("ESTADO ATUAL\tALFABETO DE ENTRADA\tPRÓXIMO ESTADO")
+        # for key, dict_value in transition_dict.items():
+        #     for input_alphabet, transition_state in dict_value.items():
+        #         print("{}\t\t{}\t\t{}".format(key, input_alphabet, transition_state))
 
         return transition_dict
 
@@ -107,7 +110,7 @@ class DFA:
         """Takes in current state and goes to next state based on input symbol."""
         if (self.CURRENT_STATE == 'REJECT'):
             return False
-        print("ESTADO ATUAL : {}\tSÍMBOLOS DE ENTRADA : {}\t PRÓXIMO ESTADO : {}".format(self.CURRENT_STATE, input_symbol,self.DELTA[self.CURRENT_STATE][input_symbol]))
+        # print("ESTADO ATUAL : {}\tSÍMBOLOS DE ENTRADA : {}\t PRÓXIMO ESTADO : {}".format(self.CURRENT_STATE, input_symbol,self.DELTA[self.CURRENT_STATE][input_symbol]))
         self.CURRENT_STATE = self.DELTA[self.CURRENT_STATE][input_symbol]
         return self.CURRENT_STATE
 
@@ -130,15 +133,17 @@ class DFA:
 
 if __name__ == "__main__":
     check = True
-    print("\nMáquina de AFD.")
+    # print("\nMáquina de AFD.")
     machine = DFA()
     while (check):
         choice = int(input("\nEscolha uma opção:\n1. Exit\n2. Execute a palavra no AFD\nDigite a sua escolha : "))
         if (choice == 1):
             sys.exit()
         elif (choice == 2):
-            palavra = machine.leituraarquivo()
-            print(palavra)
-            # print("ACEITA" if machine.run_machine(palavra) else "REJEITADA")
+            palavras = machine.leituraarquivo()
+            resultado = open("resultado.txt", "w")
+            for palavra in palavras:
+                resultado.write("{} : ACEITA\n".format(palavra) if machine.run_machine(palavra) else "{} : Erro lexico!\n".format(palavra))
+            resultado.close()
         else:
             check = False
