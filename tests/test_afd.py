22976450288ba9695a85a755b66d4d6f395eec6c
Felipe Vidal <felipeisra.vidal@gmail.com>
2022-07-12 01:00:02 -0300

    update tests

--- tests/test_afd.py
+++ tests/test_afd.py
@@ -6,16 +6,23 @@ class TestAFD(unittest.TestCase):
     def __init__(self, methodName):
         super().__init__(methodName)
 
-        self.machine = DFA()
+        self.machine = DFA([], [], [], [], [], None)
 
     def test_ABC(self):
-        self.assertTrue(True)
+        result = self.machine.run_machine('ABC')
+        self.assertTrue(result)
 
     def test_ABB(self):
-        self.assertTrue(True)
+        result = self.machine.run_machine('ABB')
+        self.assertFalse(result)
 
     def test_ABAA(self):
-        self.assertTrue(True)
+        result = self.machine.run_machine('ABAA')
+        self.assertFalse(result)
+
+    def test_ABBC(self):
+        result = self.machine.run_machine('ABBC')
+        self.assertFalse(result)
 
 if __name__ == 'main':
     unittest.main()
